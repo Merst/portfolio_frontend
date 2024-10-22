b75f5d06ca65c81bd9c3a9de5638df68356a6c93
Merst <noxlittner@gmail.com>
2024-10-21 20:07:48 -0400

    Proper configuration of nginx in container's runtime phase for production.

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,7 @@ FROM nginx:alpine
 COPY nginx.conf /etc/nginx.conf
 
 COPY --from=build-stage /app/dist /usr/share/nginx/html
+COPY resume.pdf /usr/share/nginx/html
 
 EXPOSE 80
 
--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import pdf from '../../resume.pdf'
 const ResumePage = () => {
   return (
     <>
       <h3 className="text-3xl py-5"> My Resume </h3>
       {/* Height should be relative to the viewer's screen. height="100%" doesn't seem to do the trick. */}
-      <object className='pt-1' data="../../public/resume.pdf" type="application/pdf" width="100%" height="600px" />      
+      	
+      <embed src={pdf} type="application/pdf" width="100%" height="600px" />
+      
       <p>Created using Latex</p>
     </>
   )
