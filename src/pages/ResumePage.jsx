e261fb3982105070916bc7dc792c2cefb7b82247
Merst <stephane.verville.vohl@gmail.com>
2024-10-30 12:56:50 -0400

    addition of barebone static files to repository

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -5,7 +5,6 @@ const ResumePage = () => {
     <>
       <h3 className="text-3xl py-5"> My Resume </h3>
       {/* Height should be relative to the viewer's screen. height="100%" doesn't seem to do the trick. */}
-      	
       <embed src={pdf} type="application/pdf" width="100%" height="600px" />
       
       <p>Created using Latex</p>
